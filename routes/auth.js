c46f1dfbc7b9293461b911c17f7fd868685a02c6
NITISH KUMAR <81707719+nitish-09105@users.noreply.github.com>
2023-11-02 22:07:07 +0530

    user details authentication & handling duplicacy in DB

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,46 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-
-// create a using POST "/api/auth/". Doesn't require Auth
-router.post("/", (req, res) => {
+const { body, validationResult } = require('express-validator');
+// create a using POST "/api/auth/createruser". No login required
+router.post("/createuser",[
+  body('name','Enter a valid Name').isLength({min:3}),
+  body('email','Enter a valid Email').isEmail(),
+  body('password', 'Enter a valid Password').isLength({min:5}),
+], async(req, res) => {
   // res.json(obj);
-  console.log(req.body);
+  // console.log(req.body);
+  
+  // checking errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors:errors.array()});
+  }
 
-  const user = User(req.body);
-  user.save();
-  // res.send('hello')
-  res.send(req.body);
+  // save data in mongoose
+  // const user = User(req.body);
+  // user.save();
+  // // res.send('hello')
+
+  // check whether the user with this email exists already
+
+  try{
+  let user=await User.findOne({email:req.body.email});
+  if (user){
+    return res.status(400).json({error: 'Sorry with this email already exists'})
+  }
+  user=await User.create({
+    name:req.body.name,
+    email:req.body.email,
+    password:req.body.password,
+  })
+  // res.send(req.body);
+  res.json(user);
+  }
+  catch(error){ 
+    console.error(error.message)
+    res.status(500).send('some error occured')
+  }
 });
 
 module.exports = router;
